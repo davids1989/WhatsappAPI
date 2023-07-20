7f765c27cbe7a5e57179641df718207e9c759785
davids1989 <daviddegusmao@gmail.com>
2023-07-19 22:27:39 -0300

    v2 codigo

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response} from "express"
+import express, { Request, Response } from "express"
 
 import Sender from "./sender"
 
@@ -13,21 +13,21 @@ app.get('/status', (req: Request, res: Response) => {
 })
 
 app.post("/send", async (req: Request, res: Response) => {
-    const {number, message} = req.body
+    const { number, message } = req.body
 
-// Verificar se o número de telefone e a mensagem não estão vazios
-if (!number || !message) {
-    return res.status(400).json({ status: "error", message: "Número de telefone e/ou mensagem não foram fornecidos corretamente." });
-}
+    // Verificar se o número de telefone e a mensagem não estão vazios
+    if (!number || !message) {
+        return res.status(400).json({ status: "error", message: "Número de telefone e/ou mensagem não foram fornecidos corretamente." });
+    }
     try {
-       await sender.sendText(number, message)
+        await sender.sendText(number, message)
 
-       return res.status(200).json({ status: "success", message: "Mensagem enviada com sucesso!" });
+        return res.status(200).json({ status: "success", message: "Mensagem enviada com sucesso!" });
     } catch (error) {
         console.error(error)
         return res.status(500).json({ status: "error", message: "Ocorreu um erro ao enviar a mensagem." });
     }
-    
+
 })
 
 app.listen(5000, () => {
--- src/sender.ts
+++ src/sender.ts
@@ -1,33 +1,42 @@
-import { start } from "repl";
+import parsePhoneNumber, { isValidPhoneNumber } from "libphonenumber-js";
 import { create, Whatsapp, Message, SocketState } from "venom-bot";
 
 
-class Sender{
+class Sender {
     private client: Whatsapp
 
-    constructor(){
+    constructor() {
         this.initialize()
     }
 
-    async sendText(to: string, body: string){
-        await this.client.sendText(to, body)
+    async sendText(to: string, body: string) {
+
+        if (!isValidPhoneNumber(to, "BR")) {
+            throw new Error("this number is not valid")
+        }
+
+        let phoneNumber = parsePhoneNumber(to, "BR")?.format("E.164")?.replace("+", "") as string
+        phoneNumber = phoneNumber.includes("@c.us") ? phoneNumber : `${phoneNumber}@c.us`;
+
+        console.log("phoneNumber", phoneNumber)
+        await this.client.sendText(phoneNumber, body)
     }
 
-    private initialize(){
+    private initialize() {
         const qr = (base64Qrimg: string) => {
             //return isLogged || notLogged || browserClose || qrReadSuccess || qrReadFail || autocloseCalled || 
             //desconnectedMobile || deleteToken || chatsAvailable || deviceNotConnected || serverWssNotConnected || 
             //noOpenBrowser || initBrowser || openBrowser || connectBrowserWs || initWhatsapp || erroPageWhatsapp || 
             //successPageWhatsapp || waitForLogin || waitChat || successChat
-      
+
         }
 
-        const status = (statusSession: String) => {}
+        const status = (statusSession: String) => { }
 
         const start = (client: Whatsapp) => {
             this.client = client
 
-            
+
         }
 
 
