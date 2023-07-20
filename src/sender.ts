10f9129aeb9e763619bf99c06156e5cd8097dbc2
davids1989 <daviddegusmao@gmail.com>
2023-07-20 14:56:48 -0300

    Código atualizado

--- src/sender.ts
+++ src/sender.ts
@@ -1,11 +1,34 @@
 import parsePhoneNumber, { isValidPhoneNumber } from "libphonenumber-js";
-import { create, Whatsapp, Message, SocketState } from "venom-bot";
+import { create, Whatsapp, Message, SocketState, CatchQR } from "venom-bot";
+
+export type QRCode = {
+    base64Qr: string
+    asciiQR: string
+    attempts: number
+}
 
 
 class Sender {
     private client: Whatsapp
+    private connected: boolean
+    private qr: QRCode
+    private qrAttempts: number; // Nova variável para rastrear as tentativas
+
+
+    get isConnected() : boolean {
+        return this.connected
+    }
+
+    get qrCode() : QRCode {
+        return this.qr
+    }
+    
+    get qrAttemptsCount(): number { // Método para obter o número de tentativas
+        return this.qrAttempts;
+    }
 
     constructor() {
+        this.qrAttempts = 0; // Inicializa o contador de tentativas
         this.initialize()
     }
 
@@ -16,22 +39,24 @@ class Sender {
         }
 
         let phoneNumber = parsePhoneNumber(to, "BR")?.format("E.164")?.replace("+", "") as string
-        phoneNumber = phoneNumber.includes("@c.us") ? phoneNumber : `${phoneNumber}@c.us`;
+        phoneNumber = phoneNumber.includes("@c.us") ? phoneNumber : `${phoneNumber}@c.us`
 
         console.log("phoneNumber", phoneNumber)
         await this.client.sendText(phoneNumber, body)
     }
 
     private initialize() {
-        const qr = (base64Qrimg: string) => {
-            //return isLogged || notLogged || browserClose || qrReadSuccess || qrReadFail || autocloseCalled || 
-            //desconnectedMobile || deleteToken || chatsAvailable || deviceNotConnected || serverWssNotConnected || 
-            //noOpenBrowser || initBrowser || openBrowser || connectBrowserWs || initWhatsapp || erroPageWhatsapp || 
-            //successPageWhatsapp || waitForLogin || waitChat || successChat
+        const qr: CatchQR = (qrCode: string, asciiQR: string, attempt?: number) => {
+            this.qrAttempts = attempt || 0; // Se attempt for undefined, atribui 0
+            this.qr = { base64Qr: qrCode, asciiQR: asciiQR, attempts: this.qrAttempts };
+        };
 
+        const status = (statusSession: string) => { 
+
+            this.connected = ["isLogged", "qrReadSuccess", "chatsAvailable", "successChat"].includes(
+                statusSession
+                )
         }
-
-        const status = (statusSession: String) => { }
 
         const start = (client: Whatsapp) => {
             this.client = client
